c9d2ad4132b04cb972c0be4039bc932fd23aa207
Chris Liow <c.liow@claritypartners.com>
2015-06-21 20:34:44 -0500

    Added file for QuandlAPI

--- Program.py
+++ Program.py
@@ -1,6 +1,7 @@
 import Quandl as Q
 import numpy as N
 import pytrade # this is our library, must be in same directory
+import QuandlAPI as QAPI
 
 lastMonthClosing = Q.get("YAHOO/AAPL.6", rows=30, sort_order='desc', authtoken="QyJB1_5vMdTh-GSMWar7")
 valueList = lastMonthClosing.values
@@ -8,5 +9,6 @@ print(max(valueList))
 for value in valueList:
     print(value)
 
-
+# sample use of QuandlAPI library
+# lastMonthClosing = QAPI.getClosing("AAPL", 30)
 
---
+++ QuandlAPI.py
@@ -0,0 +1,7 @@
+import Quandl as Q
+
+# this file should contain the methods for requesting data
+# we should attempt to keep the main program as clean as possible
+
+# sample function
+
