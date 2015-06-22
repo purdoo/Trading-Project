65f70899b6ae3f55a79cc7c014562377fdf55dc0
Matt Wiseman <mattwiseman@Matt-Wisemans-MacBook-Pro.local>
2015-06-21 21:43:43 -0400

    Devs now different

--- Program.py
+++ Program.py
@@ -3,6 +3,8 @@ import numpy as N
 import pytrade # this is our library, must be in same directory
 import QuandlAPI as QAPI
 
+#Dev not master
+
 lastMonthClosing = Q.get("YAHOO/AAPL.6", rows=30, sort_order='desc', authtoken="QyJB1_5vMdTh-GSMWar7")
 valueList = lastMonthClosing.values
 print(max(valueList))
