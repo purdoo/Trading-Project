c5956c0691f4dea7c8656459216198bf16c808ad
Chris Liow <c.liow@claritypartners.com>
2015-06-28 01:01:02 -0500

    solved merge issues

--- Program.py
+++ Program.py
@@ -1,18 +1,8 @@
 import Quandl as Q
 import numpy as np
-
-<<<<<<< HEAD
-#Dev not master
-
-lastMonthClosing = Q.get("YAHOO/AAPL.6", rows=30, sort_order='desc', authtoken="QyJB1_5vMdTh-GSMWar7")
-valueList = lastMonthClosing.values
-print(max(valueList))
-for value in valueList:
-    print(value)
-=======
 import QuandlAPI as QAPI
 import pytrade as pyt
->>>>>>> master
+
 
 # sample use of QuandlAPI library
 #lastMonthClosing = Q.get("YAHOO/AAPL.6", rows=30, sort_order='desc', authtoken="QyJB1_5vMdTh-GSMWar7")
